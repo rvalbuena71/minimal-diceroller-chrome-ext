92ed67abcffd7c5342ae6e9de5daccf57ccf4066
Ramon <rvalbuena71@gmail.com>
2026-05-10 16:26:35 -0400

    internal rewrites to use unique diceid instead of keying on dice name (which could change), probably needs more cleanup

--- dice.js
+++ dice.js
@@ -1,4 +1,30 @@
 
+export class DieRoller {
+
+    static roll(dice) {
+        return dice.min + Math.floor(Math.random() * (dice.max - dice.min + 1));
+    }
+
+    static rollCount(dice, count) {
+        let result = new Array(count);
+        for (let i = 0; i < count; i++) {
+            result[i] = DieRoller.roll(dice);
+        }
+        return new DiceResult(result);
+    }
+
+    static countSides(dice) {
+        return (dice.max - dice.min) +1;
+    }
+
+    static isValid(dice) {
+        return (
+            (dice.min <= dice.max) &&
+            (dice.name != undefined && dice.name != '') 
+        );
+    }
+
+}
 
 export class Dice {
 
@@ -29,9 +55,21 @@ export class DiceResult {
 	}
 
     #percentile(list) {
+        /*
        return list.reduce(
-			(accumulator, currentValue) => (accumulator * 10) + currentValue
-		);
+			(accumulator, currentValue) => 
+                (accumulator * (10 ** DiceResult.countDigits(currentValue))) + 
+                    currentValue)
+                    */
+        return Number.parseInt(
+            list.reduce(
+            (accumulator, currentValue) => accumulator + currentValue.toString()
+            )
+        );
+    }
+
+    static countDigits(num) {
+        return Math.floor(Math.log10(num) + 1);
     }
 
 }
@@ -55,20 +93,91 @@ export class DicePool {
 
 }
 
-export const diceBag = new Map();
-let d4 = new DicePool("D4", new Dice("D4", 1, 4,), 1, false);
-diceBag.set("D4", d4);
-let d6 = new DicePool("D6", new Dice("D6", 1, 6), 1, false);
-diceBag.set("D6", d6);
-let d8 = new DicePool("D8", new Dice("D8", 1, 8), 1, false);
-diceBag.set("D8", d8);
-let d10 = new DicePool("D10", new Dice("D10", 1, 10), 1, false);
-diceBag.set("D10", d10);
-let d12 = new DicePool("D12", new Dice("D12", 1, 12), 1, false)
-diceBag.set("D12", d12);
-let d20 = new DicePool("D20", new Dice("D20", 1, 20), 1, false);
-diceBag.set("D20", d20);
-let d100 = new DicePool("D100", new Dice("D100", 1, 100), 1, false);
-diceBag.set("D100", d100);
-let dF = new DicePool("DF", new Dice("DF", -1, 1), 4, false);
-diceBag.set("DF", dF);
+export const diceKeys = [
+    "dice01",
+    "dice02",
+    "dice03",
+    "dice04",
+    "dice05",
+    "dice06",
+    "dice07",
+    "dice08",
+    "dice09",
+    "dice10",
+    "dice11",
+    "dice12",
+    "dice13",
+    "dice14",
+    "dice15",
+    "dice16",
+    "dice17",
+    "dice18"
+];
+
+export const commonDice = [
+    "D4", "D6", "D8", "D10", "D12", "D20", "D100", "DF"
+];
+export const extraDice = [
+    "D2", "D3", "D5", "D7", "D14", "D16", "D24", "D30", "D60", "D120"
+];
+
+export const diceValues = [
+    {"id":"dice01", "name":"D4",  "min":1, "max":4},
+    {"id":"dice02", "name":"D6",  "min":1, "max":6},
+    {"id":"dice03", "name":"D8",  "min":1, "max":8},
+    {"id":"dice04", "name":"D10", "min":1, "max":10},
+    {"id":"dice05", "name":"D12", "min":1, "max":12},
+    {"id":"dice06", "name":"D20", "min":1, "max":20},
+    {"id":"dice07", "name":"D100","min":1, "max":100},
+    {"id":"dice08", "name":"DF",  "min":-1,"max":1},
+    {"id":"dice09", "name":"D2",  "min":1, "max":2},
+    {"id":"dice10", "name":"D3",  "min":1, "max":3},
+    {"id":"dice11", "name":"D5",  "min":1, "max":5},
+    {"id":"dice12", "name":"D7",  "min":1, "max":7},
+    {"id":"dice13", "name":"D14", "min":1, "max":14},
+    {"id":"dice14", "name":"D16", "min":1, "max":16},
+    {"id":"dice15", "name":"D24", "min":1, "max":24},
+    {"id":"dice16", "name":"D30", "min":1, "max":30},
+    {"id":"dice17", "name":"D60", "min":1, "max":60},
+    {"id":"dice18", "name":"D120","min":1, "max":120}
+];
+
+export function findDefaultDice(keyId) {
+    var result;
+    // get original value from const
+    diceValues.forEach((value) => {
+        if (value.id == keyId) {
+            result = value;
+        }
+    });
+    return result;
+}
+
+export function getDefaultDiceData(){
+    var diceMap = new Map();
+    var i = 0;
+    diceKeys.forEach((key) => {
+        diceMap.set(key,val);
+        i++
+    });
+    return diceMap;
+}
+
+
+export function getDiceDataByUniqueID(){
+    var diceMap = new Map();
+    var i = 0;
+    diceKeys.forEach((key) => {
+        var val = JSON.parse(JSON.stringify(diceValues[i]));
+        diceMap.set(key,val);
+        i++
+    });
+    return diceMap;
+}
+
+export function sortDiceMapByMax(diceMap) {
+    return new Map([...diceMap.entries()].sort((a,b) =>{
+            return a[1].max - b[1].max;
+        }))
+}
+
--- popup.js
+++ popup.js
@@ -1,54 +1,57 @@
-import { Dice, DicePool, DiceResult, diceBag } from './dice.js';
+import { DieRoller, diceKeys, diceValues, getDiceDataByUniqueID, sortDiceMapByMax, commonDice, extraDice } from './dice.js';
+import { loadAllSettings, saveAllSettings, saveOneSetting, parseFromSettings } from './common.js';
 
-function buildRow(name, dicePool) {
-    console.log(name + " : " + JSON.stringify(dicePool));
-    return `<tr id="row-${name}">
-    <td><input id="count-${dicePool.name}" value="${dicePool.count}" size="2" min="1" type="number" class="countField"></td>`+
-    `<td><button id="roll-${dicePool.name}" data-type="${dicePool.name}" class="rollbutton">${dicePool.name}</button></td>`+
-    `<td><input id="result-${dicePool.name}" size="4" class="resultField"></td>
+// diceList will be populated on page load
+var diceList;
+
+function buildRow2(keyId, dice) {
+    console.log(keyId + " : " + JSON.stringify(dice));
+    return `<tr id="${keyId}">
+    <td><input id="count-${keyId}" value="1" size="2" min="1" type="number" class="countField"></td>`+
+    `<td><button id="roll-${keyId}" data-type="${dice.name}" class="rollbutton">${dice.name}</button></td>`+
+    `<td class="checkCell" ><input type="checkbox" class="checkbox" id="percentile-${keyId}"></td>`+
+    `<td><input id="result-${keyId}" size="4" class="resultField"></td>
     </tr>`;
-}
+};
 
 function buildLogWindow() {
     return `<tr>
       <td colspan="2">Dice Pool</td>
-      <td><button id="clearAll">Clear</button></td>
+      <td colspan="2"><button id="clearAll" title="Clear all results fields">Clear All</button></td>
     </tr>
     <tr>
-      <td colspan="3"><input id="logwindow"></input></td>
+      <td colspan="4"><input id="logwindow"></input></td>
     </tr>`;
-}
+};
 
-function buildButtonTable() {
-    return `<table class="buttonTable">
-    <tr>
-      <td><button id="clearAll">Clear</button></td>
-      <td><button id="resetAll">Reset</button></td>
-    </tr>
-  </table>`;
-}
-
-function buildMainTable() {
+function buildMainTable(diceData,settings) {
+    // safety check
+    var targetDiv = document.getElementById('mainDiv');
+    if (targetDiv == undefined) return;
     console.log("buildMainTable START");
+    console.log(diceData);
     let tableHtml = 
     `<table class="mainTable">`+
     `<tr>
-      <td>#</td>
+      <td title="Number of dice to roll">#</td>
       <td>Type</td>
+      <td class="centeredText" title="Treat results as 'percentile dice'">%</td>
       <td>Result</td>
     </tr>`;
-
-    diceBag.forEach((value,key) => {
-        tableHtml += buildRow(key, value);
+    diceData.forEach((value,key) => {
+        let showRow = settings.get(`show-${key}`);
+        if (showRow != undefined && showRow == true) {
+            tableHtml += buildRow2(key, value);
+        }
     });
 
     tableHtml += buildLogWindow();
     tableHtml += `</table>`;
     //tableHtml += buildButtonTable();
     //console.log(tableHtml);
-    document.getElementById('mainDiv').innerHTML = tableHtml;
+    targetDiv.innerHTML = tableHtml;
     console.log("buildMainTable END");
-}
+};
 
 
 function clearAllButtonClickHandler() {
@@ -56,23 +59,33 @@ function clearAllButtonClickHandler() {
     let resultArray = document.getElementsByClassName('resultField');
     for (let i = 0; i < resultArray.length; i++) {
         resultArray[i].value = '';
+        resultArray[i].dispatchEvent(new Event('input', { bubbles: true }));
     }
-    document.getElementById('logwindow').value = '';
-}
+    var logwindow = document.getElementById('logwindow');
+    if (logwindow != undefined) {
+        logwindow.value = '';
+        logwindow.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+};
 
 function rollButtonClickHandler(buttonElement) {
-    // data-type tag determines what dice type to roll (D6, D10, D20, etc)
-    // and matches naming convention of input controls that go with the clicked button
+    // use data-type tag to find input controls that go with the clicked button
     var diceType = buttonElement.dataset.type;
+    var keyId = buttonElement.closest("tr").id;
+    console.log(keyId);
     // input box gives us the number of dice to roll (defaults to 1)
-    var num = Number.parseInt(document.getElementById('count-' + diceType).value);
-    var dicePool = diceBag.get(diceType);
-    dicePool.count = num;
-    var diceResult = dicePool.roll();
-   document.getElementById('result-' + diceType).value = (dicePool.isPercentile == true) ? diceResult.percent : diceResult.total;
-   // show the individual rolls
-   document.getElementById('logwindow').value = diceResult.rolls;
-}
+    var num = Number.parseInt(document.getElementById('count-' + keyId).value);
+    var dice = diceList.get(keyId);
+    var diceResult = DieRoller.rollCount(dice,num);
+    var isPercentile = document.getElementById('percentile-' + keyId).checked;
+    var resultField = document.getElementById('result-' + keyId);
+    resultField.value = (isPercentile == true) ? diceResult.percent : diceResult.total;
+    resultField.dispatchEvent(new Event('input', { bubbles: true }));
+    var logwindow = document.getElementById('logwindow');
+    // show the individual rolls
+    logwindow.value = diceResult.rolls;
+    logwindow.dispatchEvent(new Event('input', { bubbles: true }));
+};
 
 function hookClickEvents() {
     //console.log("hookClickEvents");
@@ -83,42 +96,160 @@ function hookClickEvents() {
             rollButtonClickHandler(event.target);
         });
     }
-    document.getElementById('clearAll').addEventListener("click", (event) => {
+    var clearAll = document.getElementById('clearAll');
+    if (clearAll != undefined) {
+        clearAll.addEventListener("click", (event) => {
             clearAllButtonClickHandler();
-    });
+        });
+    }
 };
 
-function hookCountChangeEvents() {
-    //console.log("hookCountChangeEvents");
+
+function assignCountFieldValues(storedSettings) {
+    let countArray = document.getElementsByClassName('countField');
+    //console.log(countArray);
+    for (let i = 0; i < countArray.length; i++) {
+        let fieldVal = storedSettings.get(countArray[i].id);
+        //console.log(countArray[i].id);
+        //console.log(fieldVal);
+        if (fieldVal != undefined) {
+            countArray[i].value = fieldVal;
+        } 
+    }
+};
+
+function assignResultFieldValues(storedSettings) {
+    let isSaveResults = storedSettings.get('save-results');
+    console.log(storedSettings.get("save-results"));
+    if (isSaveResults == undefined || isSaveResults == false) return;
+    let resultArray = document.getElementsByClassName('resultField');
+    //console.log(countArray);
+    for (let i = 0; i < resultArray.length; i++) {
+        let fieldVal = storedSettings.get(resultArray[i].id);
+        //console.log(resultArray[i].id);
+        //console.log(fieldVal);
+        if (fieldVal != undefined) {
+            resultArray[i].value = fieldVal;
+        } 
+    }
+    let logwindowVal = storedSettings.get("logwindow");
+    if (logwindowVal != undefined) {
+        let logwindow = document.getElementById('logwindow');
+        if (logwindow != undefined) {
+            logwindow.value = logwindowVal;
+        }
+    }
+};
+
+function hookChangeEvents(storedSettings) {
+    console.log("hookChangeEvents");
     let countArray = document.getElementsByClassName('countField');
     for (let i = 0; i < countArray.length; i++) {
         countArray[i].addEventListener("change", (event) => {
             // save when countField value is changed
-            // square brackets around keyname to use variable value
-            chrome.storage.sync.set({ [event.target.id] : event.target.value }).then(() => {
-                console.log("changed::"+event.target.id+":"+event.target.value);
-            }).catch(error => console.log(error));
+            saveOneSetting(event.target.id, event.target.value);
         });
     }
-}
-
-async function loadStoredValues() {
-    //console.log("loadStoredValues");
-    let countArray = document.getElementsByClassName('countField');
-    // get all the stored values in one go
-    chrome.storage.sync.get().then((items) => {
-        //console.log(items);
-        // assign valid stored values to the corresponding field
-        for (const [key, value] of Object.entries(items)) {
-            console.log("loaded::"+key+":" + value);
-            if (value != undefined && value > 0) {
-                countArray[key].value = value;
-            }
+    let checkboxArray = document.getElementsByClassName('checkbox');
+    for (let i = 0; i < checkboxArray.length; i++) {
+        checkboxArray[i].addEventListener("change", (event) => {
+            // save when checkbox is changed
+            saveOneSetting(event.target.id, event.target.checked);
+        });
+    }
+    let isSaveResults = storedSettings.get("save-results");
+    if (isSaveResults != undefined && isSaveResults == true) {
+        let resultArray = document.getElementsByClassName('resultField');
+        for (let i = 0; i < resultArray.length; i++) {
+            resultArray[i].addEventListener("input", (event) => {
+                // save when resultArray value is changed
+                saveOneSetting(event.target.id, event.target.value);
+            });
         }
-    }).catch(error => console.log(error));    
-}
+        let logwindow = document.getElementById('logwindow');
+        if (logwindow != undefined) {
+            logwindow.addEventListener("input", (event) => {
+                // save when logwindow value is changed
+                saveOneSetting(event.target.id, event.target.value);
+            });
+        }
+    }
+};
 
-loadStoredValues();
-buildMainTable();
-hookClickEvents();
-hookCountChangeEvents();
+function assignCheckboxValues(storedSettings) {
+    let checkboxArray = document.getElementsByClassName('checkbox');
+    for (let i = 0; i < checkboxArray.length; i++) {
+        let fieldVal = storedSettings.get(checkboxArray[i].id); 
+        //console.log(checkboxArray[i].id);
+        //console.log(fieldVal);
+        if (fieldVal != undefined) {
+            checkboxArray[i].checked = fieldVal;
+        }
+    }
+};
+
+function assignValues(storedSettings) {
+    console.log("assignValues");
+    assignCountFieldValues(storedSettings);
+    assignCheckboxValues(storedSettings);
+    assignResultFieldValues(storedSettings);
+    return storedSettings;
+};
+
+function buildPopupUI(settings){
+    return new Promise((resolve) => {
+        // sort the dice list by "max" value
+        diceList = sortDiceMapByMax(getDiceDataByUniqueID());
+        console.log(diceList);
+        addDefaultKeys(diceList,settings);
+        buildMainTable(diceList,settings);
+        assignValues(settings);
+        hookClickEvents(settings);
+        hookChangeEvents(settings);
+        resolve(settings);
+    });
+};
+
+// check for new settings and add them if necessary
+export function addDefaultKeys(diceList, settings){
+    console.log("addDefaultKeys");
+    diceList.forEach((dice) => {
+        let checkKey = dice.id;
+        // is there a copy of this dice data in storage?
+        let savedDice = settings.get(checkKey);
+        if (savedDice == undefined) {
+            // 
+            saveOneSetting(checkKey,dice);
+            settings.set(checkKey,dice);
+        }
+        else {
+            console.log("addDefaultKeys - overrding defaults :"+checkKey +": "+JSON.stringify(savedDice));
+            // copy any user-edited settings onto the dice
+            dice.name = savedDice.name;
+            dice.min = Number.parseInt(savedDice.min);
+            dice.max = Number.parseInt(savedDice.max);
+        }
+    });
+    if (!settings.has("save-results")) {
+        saveOneSetting("save-results",true);
+        settings.set("save-results",true);
+    }
+    let counter = 0;
+    diceKeys.forEach((key) => {
+        // should this dice be shown?
+        let checkKey = `show-${key}`;
+        if (!settings.has(checkKey)) {
+            if (counter > 8) return;
+            saveOneSetting(checkKey,true);
+            settings.set(checkKey,true);
+        }
+        counter++;
+    });
+};
+
+loadAllSettings()
+    .then(function(result){ 
+        console.log(result);
+        return buildPopupUI(result)
+    })
+    .catch(error => console.log(error));
